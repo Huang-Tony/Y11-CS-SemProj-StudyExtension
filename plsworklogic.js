439d711c04bc7d708664063ba7f0db1752526d70
Huang-Tony <tonyh2027@student.cis.edu.hk>
2024-12-13 13:44:25 +0800

    added boilerplate

---
+++ plsworklogic.js
@@ -0,0 +1,9 @@
+console.log("Pop-up popped up");
+element.addEventListener("Add_Bookmark_Button", myFunction);
+let ActiveBookmarks = {};
+let bookmarkCount = 0;
+function Addbookmark{
+    map['bookmark' + bookmarkCount] = 'enter bookmark'
+console.log("Added Bookmark")
+
+}
