9558f20ead59b0abb3264a2c7f47343900f6ba98
Huang-Tony <tonyh2027@student.cis.edu.hk>
2024-12-20 00:00:57 +0800

    Functional input + settings page + mid css

--- plsworklogic.js
+++
@@ -1,9 +0,0 @@
-console.log("Pop-up popped up");
-element.addEventListener("Add_Bookmark_Button", myFunction);
-let ActiveBookmarks = {};
-let bookmarkCount = 0;
-function Addbookmark{
-    map['bookmark' + bookmarkCount] = 'enter bookmark'
-console.log("Added Bookmark")
-
-}
